5728cc7f94e9df2b871e1e6328ef3637380b78b9
Tamer Mahmoud <tamer.mahmoud@expel.io>
2023-02-02 20:06:31 -0500

    Completed Part 1 of ember.js tutorial. Proceeding to Part 2..

---
+++ tests/acceptance/canine-parks-test.js
@@ -0,0 +1,67 @@
+import { module, test } from 'qunit';
+import { click, visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'canine-parks/tests/helpers';
+
+module('Acceptance | canine parks', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /canine-parks', async function (assert) {
+    await visit('/');
+
+    assert.strictEqual(currentURL(), '/');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('Canine Parks');
+    assert.dom('h2').hasText('Welcome to Canine Parks!');
+
+    assert.dom('.jumbo a.button').hasText('About Us');
+    await click('.jumbo a.button');
+
+    assert.strictEqual(currentURL(), '/about');
+  });
+
+  test('visiting /about', async function (assert) {
+    await visit('/about');
+
+    assert.strictEqual(currentURL(), '/about');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('Canine Parks');
+    assert.dom('h2').hasText('About Canine Parks');
+
+    assert.dom('.jumbo a.button').hasText('Contact Us');
+    await click('.jumbo a.button');
+
+    assert.strictEqual(currentURL(), '/getting-in-touch');
+  });
+
+  test('visiting /getting-in-touch', async function (assert) {
+    await visit('/getting-in-touch');
+
+    assert.strictEqual(currentURL(), '/getting-in-touch');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('Canine Parks');
+    assert.dom('h2').hasText('Contact Us');
+
+    assert.dom('.jumbo a.button').hasText('About Us');
+    await click('.jumbo a.button');
+
+    assert.strictEqual(currentURL(), '/about');
+  });
+
+  test('navigating using the nav-bar', async function (assert) {
+    await visit('/');
+
+    assert.dom('nav').exists();
+    assert.dom('nav a.menu-index').hasText('Canine Parks');
+    assert.dom('nav a.menu-about').hasText('About');
+    assert.dom('nav a.menu-contact').hasText('Contact');
+
+    await click('nav a.menu-about');
+    assert.strictEqual(currentURL(), '/about');
+
+    await click('nav a.menu-contact');
+    assert.strictEqual(currentURL(), '/getting-in-touch');
+
+    await click('nav a.menu-index');
+    assert.strictEqual(currentURL(), '/');
+  });
+});
